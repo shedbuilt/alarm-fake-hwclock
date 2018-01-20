49c4ff709c2bbeac68d6e90f5d14babf93bf51a2
Auston Stewart <auston@shedbuilt.net>
2018-01-20 07:36:59 -1000

    Using newer shedmake git refs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-install -dm755 ${SHED_FAKEROOT}/lib/systemd/system
-install -m644 ${SHED_SRCDIR}/systemd/fake-hwclock.service ${SHED_FAKEROOT}/lib/systemd/system/
+make -j $SHED_NUMJOBS && \
+make DESTDIR="$SHED_FAKEROOT" install && \
+install -dm755 "${SHED_FAKEROOT}/lib/systemd/system" && \
+install -m644 "${SHED_SRCDIR}/systemd/fake-hwclock.service" "${SHED_FAKEROOT}/lib/systemd/system/"
