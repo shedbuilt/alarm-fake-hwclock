664eba04a3a0047e5fa1728e9a3f3ff4a9f19f12
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:21:57 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install && \
-install -dm755 "${SHED_FAKEROOT}/lib/systemd/system" && \
-install -m644 "${SHED_SRCDIR}/systemd/fake-hwclock.service" "${SHED_FAKEROOT}/lib/systemd/system/"
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install && \
+install -dm755 "${SHED_FAKE_ROOT}/lib/systemd/system" && \
+install -m644 "${SHED_PKG_SOURCE_DIR}/systemd/fake-hwclock.service" "${SHED_FAKE_ROOT}/lib/systemd/system/"
